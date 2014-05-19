63d0f855523a99992f0dc2316154458e6155535f
Gary Fredericks <fredericksgary@gmail.com>
2014-05-19 06:25:37 -0500

    Upgrade clojure dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/fredericksgary/catch-data"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[lein-release "1.0.5"]]
   :lein-release {:deploy-via :clojars})
