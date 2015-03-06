d726119784b254374c2a1bb32a4a536491fd5b93
Gary Fredericks <fredericksgary@gmail.com>
2015-03-05 20:37:47 -0600

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/catch-data "0.2.0"
+(defproject com.gfredericks/catch-data "0.2.1-SNAPSHOT"
   :description "Clojure data-based exception handling."
   :url "https://github.com/fredericksgary/catch-data"
   :license {:name "Eclipse Public License"
